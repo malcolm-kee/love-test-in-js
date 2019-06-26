0381faddd441733c9d521641f6200508a6d6aab1
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-06-26 10:18:18 +0800

    update testing quotient to my exp

--- components/testing-quotient.jsx
+++ components/testing-quotient.jsx
@@ -10,16 +10,27 @@ const labelStyles = {
 const cellStyles = {
   textAlign: 'center',
   padding: 16,
-  border: '1px solid #aaa'
+  border: '1px solid #aaa',
+  position: 'relative'
 };
 
-const highlightedCellStyles = {
-  textAlign: 'center',
-  padding: 16,
-  border: '1px solid #aaa',
-  backgroundColor: '#60ebbf',
-  fontWeight: 'bolder'
+const circleStyles = {
+  display: 'inline-block',
+  borderRadius: '50%',
+  fontSize: '0.8em',
+  position: 'absolute',
+  backgroundColor: 'hsl(12, 78%, 25%)',
+  color: '#fff',
+  fontWeight: 'normal',
+  top: 8,
+  right: 8,
+  width: '1.5em',
+  height: '1.5em'
 };
+
+const Circle = ({ children }) => (
+  <div style={circleStyles}>{children}</div>
+);
 
 const TestingQuotient = ({ deck, multiStep }) => {
   React.useEffect(() => {
@@ -27,6 +38,8 @@ const TestingQuotient = ({ deck, multiStep }) => {
       deck.update(updaters.setSteps(deck.index, 1));
     }
   }, []);
+
+  const isStep1 = deck.step === 1;
 
   return (
     <table>
@@ -40,29 +53,25 @@ const TestingQuotient = ({ deck, multiStep }) => {
       <tbody>
         <tr>
           <td style={labelStyles}>❤️Love Test</td>
-          <td style={cellStyles}>A</td>
-          <td style={cellStyles}>B</td>
+          <td style={cellStyles}>
+            {isStep1 && <Circle>2</Circle>} A
+          </td>
+          <td style={cellStyles}>
+            {isStep1 && <Circle>4</Circle>} B
+          </td>
         </tr>
         <tr>
           <td style={labelStyles}>😐No Feeling</td>
-          <td style={cellStyles}>C</td>
-          <td
-            style={
-              deck.step === 1 ? highlightedCellStyles : cellStyles
-            }
-          >
-            D
+          <td style={cellStyles}>
+            {isStep1 && <Circle>1</Circle>} C
           </td>
+          <td style={cellStyles}>D</td>
         </tr>
         <tr>
           <td style={labelStyles}>🙅Hate Test</td>
           <td style={cellStyles}>E</td>
-          <td
-            style={
-              deck.step === 1 ? highlightedCellStyles : cellStyles
-            }
-          >
-            F
+          <td style={cellStyles}>
+            {isStep1 && <Circle>3</Circle>} F
           </td>
         </tr>
       </tbody>
